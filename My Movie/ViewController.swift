44140c70d345af4ef3d15b49622e7e54a271f86e
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2023-06-18 11:23:21 +0300

    Project Files

---
+++ My Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Movie
+//
+//  Created by Murad Farhat on 18/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
