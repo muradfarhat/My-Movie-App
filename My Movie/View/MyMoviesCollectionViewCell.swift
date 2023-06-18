537946f1316163a5fddd0145bfaf3d2f656da9c6
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2023-06-18 12:09:26 +0300

    Movie card xib

---
+++ My Movie/View/MyMoviesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesCollectionViewCell.swift
+//  My Movie
+//
+//  Created by Murad Farhat on 18/06/2023.
+//
+
+import UIKit
+
+class MyMoviesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
